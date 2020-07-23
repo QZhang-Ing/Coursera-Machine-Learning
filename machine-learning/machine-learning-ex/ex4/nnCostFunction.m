4c8d80af127d70e856ac35b0ac81ec2a327641b0
QZhang <65087759+QZhang-Ing@users.noreply.github.com>
2020-07-23 22:32:54 +0200

    update backpropagation

--- machine-learning/machine-learning-ex/ex4/nnCostFunction.m
+++ machine-learning/machine-learning-ex/ex4/nnCostFunction.m
@@ -42,27 +42,26 @@ Theta2_grad = zeros(size(Theta2));
 
 % FP
 % Add ones to the X data matrix
-X = [ones(m,1) X];
-z2 = X * Theta1' % also Thetal1 * X'
+z2 = [ones(m,1) X] * Theta1'; % also Thetal1 * X'
 a2 = (sigmoid(z2));
 
 % Add ones to the hidden layer
 a2 = [ones(m, 1) a2];
-z3 = a2 * Theta2'
-a3 = sigmoid(z3)
-h_theta = a3 % a3 is our output layer so h(x) == a3
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+h_theta = a3; % a3 is our output layer so h(x) == a3
 
 % Initialize the y(k). y(k) should contain only 0 or 1
-yVector = zeros(m, num_labels) % 10 * 5000
+yVector = zeros(m, num_labels); % 10 * 5000
 for i = 1:m
     yVector(i,y(i)) = 1;
 end
 
-J = 1/m * sum(sum((-1 * yVector .* log(h_theta)) - (1 - yVector) .* log(1 - h_theta)))
+J = 1/m * sum(sum((-1 * yVector .* log(h_theta)) - (1 - yVector) .* log(1 - h_theta)));
 
-regu = lambda/(2*m) * (sum(sum(Theta1(:, 2:end).^2)) + sum(sum(Theta2(:, 2:end).^2)))
+regu = lambda/(2*m) * (sum(sum(Theta1(:, 2:end).^2)) + sum(sum(Theta2(:, 2:end).^2)));
 
-J = J + regu
+J = J + regu;
 
 
 
@@ -82,53 +81,58 @@ J = J + regu
 %   first time.
 
 
-% For the input layer, where l=1:
-a1 = [ones(m,1) X] % 5000*401
+% Theta1 (25,401) Theta2 (10,26) X (5000,400) Y (5000,1) 
+% a1 (1,401)  z2 (1,25) a2 (1,26) z3 (1,10) a3 (1,10)
+    
 
-% For the hidden layers, where l=2:
+
+% For the input layer, where l=1:
+a1 = [ones(m,1) X]; % 5000*401
+
+% % For the hidden layers, where l=2:
 z2 = a1 * Theta1'; % 5000*401 * 401*25 = 5000*25
 a2 = [ones(m,1) sigmoid(z2)]; % add a bias 5000*26
-
+% 
 z3 = a2 * Theta2'; % 5000*26 * 26*10 = 5000*10
 a3 = sigmoid(z3); % output layer h(theta) 5000*10
-
+% 
 yy = [1:num_labels]==y; % 5000*10
-% For the delta values:
+% % For the delta values:
 delta_3 = a3 - yy; % 5000*10
-
-% (5000*10 * 25*10) .* 5000*25 = 5000*25
+% 
+% % (5000*10 * 25*10) .* 5000*25 = 5000*25
 delta_2 = (delta_3 * Theta2(:, 2:end)) .* sigmoidGradient(z2);
-
-% delta_1 is not calculated because we do not associate error with the input    
-
-% Big delta update
-% 25*401 + 25*5000 * 5000*401 
+% 
+% % delta_1 is not calculated because we do not associate error with the input    
+% 
+% % Big delta update
+% % 25*401 + 25*5000 * 5000*401 
 Theta1_grad = Theta1_grad + delta_2' * a1;
-% 10*26 + 10*5000 * 5000*26 
+% % 10*26 + 10*5000 * 5000*26 
 Theta2_grad = Theta2_grad + delta_3' * a2; 
-
-
+% 
+% 
 Theta2_grad = (1/m) * Theta2_grad; % (10*26)
 Theta1_grad = (1/m) * Theta1_grad; % (25*401)
-
-
-
-% Part 3: Implement regularization with the cost function and gradients.
-%
-%         Hint: You can implement this around the code for
-%               backpropagation. That is, you can compute the gradients for
-%               the regularization separately and then add them to Theta1_grad
-%               and Theta2_grad from Part 2.
-
-% Regularization
-
-% Theta1_grad(:, 1) = Theta1_grad(:, 1) ./ m; % for j = 0
-% Theta2_grad(:, 1) = Theta2_grad(:, 1) ./ m; % for j = 0
-
-Theta1_grad = (1/m) * Theta1_grad + (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
-Theta2_grad = (1/m) * Theta2_grad + (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
-
-% Unroll gradients
+% 
+% 
+% 
+% % Part 3: Implement regularization with the cost function and gradients.
+% %
+% %         Hint: You can implement this around the code for
+% %               backpropagation. That is, you can compute the gradients for
+% %               the regularization separately and then add them to Theta1_grad
+% %               and Theta2_grad from Part 2.
+% 
+% % Regularization
+% 
+%Theta1_grad(:, 1) = Theta1_grad(:, 1) ./ m; % for j = 0
+%Theta2_grad(:, 1) = Theta2_grad(:, 1) ./ m; % for j = 0
+% 
+Theta1_grad = Theta1_grad + (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
+Theta2_grad = Theta2_grad + (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
+% 
+% % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
 
 end
